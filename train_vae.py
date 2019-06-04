0d997b980cafe6f8cd6282155bdd7eb89cf47487
kyunster <crit9311@gmail.com>
2019-06-04 15:18:19 +0900

    vae+norm

--- train_vae.py
+++ train_vae.py
@@ -39,7 +39,7 @@ lr_d = 0.00001
 change_epoch = 2000
 save_per_epoch = 5
 
-adv_coef = 1.0; spk_coef = 10.0; cyc_coef = 5.0; con_coef = 1.0
+adv_coef = 1.0; spk_coef = 3.0; cyc_coef = 10.0; con_coef = 1.0; norm_coef=0.001
 #####################################################################
 torch.cuda.empty_cache()
 os.system("mkdir -p "+ model_dir +"/parm")
@@ -74,35 +74,38 @@ dev_loader = DataLoader(dev_set, batch_size=batch_size*2)
 
 VAE_E = net.Generator_CNN(feat_dim=120, aux_dim=100)
 VAE_G = net.Generator_CNN(feat_dim=120, aux_dim=100)
-VAE_D = net.VAE_Discriminator(feat_dim=120, hidden_dim=512, spk_dim=spk_dim)
+VAE_D_tf = net.Discriminator_MLP(feat_dim=120, hidden_dim=512)
+VAE_D_spk = net.Discriminator_CNN(feat_dim=120, frame_dim=128, hidden_dim=512, spk_dim=spk_dim)
 
 E_opt = optim.Adam(VAE_E.parameters(), lr=lr_e)
 G_opt = optim.Adam(VAE_G.parameters(), lr=lr_g)
-D_opt = optim.Adam(VAE_D.parameters(), lr=lr_d)
+Dtf_opt = optim.Adam(VAE_D_tf.parameters(), lr=lr_d)
+Dspk_opt = optim.Adam(VAE_D_spk.parameters(), lr=lr_d)
 
 E_sch = scheduler.StepLR(E_opt, step_size=100, gamma=0.5)
 G_sch = scheduler.StepLR(G_opt, step_size=100, gamma=0.5)
-D_sch = scheduler.StepLR(D_opt, step_size=100, gamma=0.5)
+Dtf_sch = scheduler.StepLR(Dtf_opt, step_size=100, gamma=0.5)
+Dspk_sch = scheduler.StepLR(Dspk_opt, step_size=100, gamma=0.5)
 
-for model in [VAE_E, VAE_G, VAE_D]:
+for model in [VAE_E, VAE_G, VAE_D_tf, VAE_D_spk]:
     model.cuda()
 torch.save(VAE_E, model_dir+"/init.pt")
 
 # Cycle Phase
 print("Cycle GAN phase")
 lm = LogManager()
-for stype in ["D_adv", "D_spk", "G_adv", "G_spk", "cyc", "content"]:
+for stype in ["D_adv", "D_spk", "G_adv", "G_spk", "cyc", "content", "D_norm", "G_norm"]:
     lm.alloc_stat_type(stype)
 
 for epoch in range(epochs):
     print("EPOCH :", epoch)
     # coefficient change
     if epoch >= change_epoch:
-        for sch in [E_sch, G_sch, D_sch]:
+        for sch in [E_sch, G_sch, Dtf_sch, Dspk_sch]:
             sch.step()
     # Train
     lm.init_stat()
-    for model in [VAE_E, VAE_G, VAE_D]:
+    for model in [VAE_E, VAE_G, VAE_D_tf, VAE_D_spk]:
         model.train()
     # D & SPK phase
     for spk_ids, idxs in train_loader:
@@ -128,23 +131,29 @@ for epoch in range(epochs):
         cB = VAE_E(xB, iB)
         xB2A = VAE_G(cB, iA)
         
-        tf_xA, spk_xA = VAE_D(xA)
-        tf_xB, spk_xB = VAE_D(xB)
-        tf_xA2B, spk_xA2B = VAE_D(xA2B)
-        tf_xB2A, spk_xB2A = VAE_D(xB2A)
+        tf_xA = VAE_D_tf(xA); spk_xA = VAE_D_spk(xA)
+        tf_xB = VAE_D_tf(xB); spk_xB = VAE_D_spk(xB)
+        tf_xA2B = VAE_D_tf(xA2B); spk_xA2B = VAE_D_spk(xA2B)
+        tf_xB2A = VAE_D_tf(xB2A); spk_xB2A = VAE_D_spk(xB2A)
+        spk_cA = VAE_D_spk(cA)
+        spk_cB = VAE_D_spk(cB)
 
         D_adv = torch.mean(tf_xA2B) - torch.mean(tf_xA) + torch.mean(tf_xB2A) - torch.mean(tf_xB)
         D_spk = nllloss(spk_xA, tA) + nllloss(spk_xB, tB) + nllloss(spk_xA2B, tA) + nllloss(spk_xB2A, tB)
+        D_norm = nllloss(spk_cA, tA) + nllloss(spk_cB, tB)
 
         # Update Parameter
-        total = adv_coef * D_adv + spk_coef * D_spk
-        D_opt.zero_grad()
+        total = adv_coef * D_adv + spk_coef * D_spk + norm_coef * D_norm
+        for opt in [Dtf_opt, Dspk_opt]:
+            opt.zero_grad()
         total.backward()
-        D_opt.step()
-
+        for opt in [Dtf_opt, Dspk_opt]:
+            opt.step()
+        
         # Save to Log
         lm.add_torch_stat("D_adv", D_adv)
         lm.add_torch_stat("D_spk", D_spk)
+        lm.add_torch_stat("D_norm", D_norm)
     # G phase
     for spk_ids, idxs in train_loader:
         total_len = len(spk_ids)
@@ -169,14 +178,17 @@ for epoch in range(epochs):
         cB = VAE_E(xB, iB)
         xB2A = VAE_G(cB, iA)
         
-        tf_xA, spk_xA = VAE_D(xA)
-        tf_xB, spk_xB = VAE_D(xB)
-        tf_xA2B, spk_xA2B = VAE_D(xA2B)
-        tf_xB2A, spk_xB2A = VAE_D(xB2A)
+        tf_xA = VAE_D_tf(xA); spk_xA = VAE_D_spk(xA)
+        tf_xB = VAE_D_tf(xB); spk_xB = VAE_D_spk(xB)
+        tf_xA2B = VAE_D_tf(xA2B); spk_xA2B = VAE_D_spk(xA2B)
+        tf_xB2A = VAE_D_tf(xB2A); spk_xB2A = VAE_D_spk(xB2A)
+        spk_cA = VAE_D_spk(cA)
+        spk_cB = VAE_D_spk(cB)
 
         G_adv = torch.mean(tf_xA) - torch.mean(tf_xA2B) + torch.mean(tf_xB) - torch.mean(tf_xB2A)
         G_spk = nllloss(spk_xA, tA) + nllloss(spk_xB, tB) +nllloss(spk_xA2B, tB) + nllloss(spk_xB2A, tA)
-        
+        G_norm = - nllloss(spk_cA, tA) - nllloss(spk_cB, tB)
+
         # phase 2: A2B -> A2B2A
         cA2B = VAE_E(xA2B, iB) 
         xA2B2A = VAE_G(cA2B, iA)
@@ -187,7 +199,7 @@ for epoch in range(epochs):
         con_cyc = l1loss(cA2B, cA) + l1loss(cB2A, cB)
         
         # Update Parameter
-        total = adv_coef * G_adv + spk_coef * G_spk + cyc_coef * cyc + con_coef * con_cyc 
+        total = adv_coef * G_adv + spk_coef * G_spk + cyc_coef * cyc + con_coef * con_cyc + norm_coef * G_norm
         for opt in [E_opt, G_opt]:
             opt.zero_grad()
         total.backward()
@@ -199,6 +211,7 @@ for epoch in range(epochs):
         lm.add_torch_stat("G_spk", G_spk)
         lm.add_torch_stat("cyc", cyc)
         lm.add_torch_stat("content", con_cyc)
+        lm.add_torch_stat("G_norm", G_norm)
 
     # Print total log of train phase
     print("Train G > \n\t", end='')
@@ -229,13 +242,15 @@ for epoch in range(epochs):
             cB = VAE_E(xB, iB)
             xB2A = VAE_G(cB, iA)
             
-            tf_xA, _ = VAE_D(xA)
-            tf_xB, _ = VAE_D(xB)
-            tf_xA2B, _ = VAE_D(xA2B)
-            tf_xB2A, _ = VAE_D(xB2A)
+            tf_xA = VAE_D_tf(xA)
+            tf_xB = VAE_D_tf(xB)
+            tf_xA2B = VAE_D_tf(xA2B)
+            tf_xB2A = VAE_D_tf(xB2A)
+            spk_cA = VAE_D_spk(cA)
+            spk_cB = VAE_D_spk(cB)
 
-            G_adv = torch.mean(tf_xA) - torch.mean(tf_xA2B) + torch.mean(tf_xB) - torch.mean(tf_xB2A)
-            D_adv = -1 * G_adv
+            adv = torch.mean(tf_xA) - torch.mean(tf_xA2B) + torch.mean(tf_xB) - torch.mean(tf_xB2A)
+            norm = nllloss(spk_cA, tA) + nllloss(spk_cB, tB)
             
             cA2B = VAE_E(xA2B, iB) 
             xA2B2A = VAE_G(cA2B, iA)
@@ -246,8 +261,8 @@ for epoch in range(epochs):
             con_cyc = l1loss(cA2B, cA) + l1loss(cB2A, cB)
 
             # Save to Log
-            lm.add_torch_stat("D_adv", D_adv)
-            lm.add_torch_stat("G_adv", G_adv)
+            lm.add_torch_stat("G_adv", adv)
+            lm.add_torch_stat("G_norm", norm)
             lm.add_torch_stat("cyc", cyc)
             lm.add_torch_stat("content", con_cyc)
 
